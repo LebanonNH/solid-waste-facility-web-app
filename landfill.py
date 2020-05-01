b180ee19cfe6d66ca97790bd87c2645584693c10
Chuck McAndrew <chuck.mcandrew@leblibrary.com>
2020-05-01 15:39:32 -0400

    initial commit

---
+++ landfill.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+charges = [{'name': "Household Bag", "Unit": "Bag", "Cost": "1"},
+           {'name': "Contractor Bag", "Unit": "Bag", "Cost": "1"},
+           {'name': "Microwave", "Unit": "Each", "Cost": "4"}
+           ]
+
+@app.route('/')
+def home():
+    return render_template('home.html', charges=charges)
+
+@app.route('/test')
+def test():
+    return render_template('barcode.html')
+
+if __name__=="__main__":
+    app.run('0.0.0.0', debug=True, ssl_context='adhoc')
